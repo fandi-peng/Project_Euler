eddc6be94fc817e413c9d4d1d262006cc2748767
fandi <fandi@jiome.(none)>
2013-07-23 13:12:40 -0400

    firt 300 in China

--- code/euler77.py
+++ code/euler77.py
@@ -1,5 +1,36 @@
 
 import pyprimes
 
+def memo(f): # to cache 
+    cache = {}
+    def _f(*args):
+        try:
+            return cache[args]
+        except KeyError:
+            cache[args] = result = f(*args) 
+            return result
+    return _f
+
+@memo
+def w(t, c, l):
+    if t == 0 or c == t == 2:
+        return 1
+    elif c == 2 != t:
+        if t % c == 0:
+            return 1
+        return 0
+    else:
+        i = t / c
+        return sum(w(t - j * c, l[l.index(c)-1], l) for j in range(i+1))
+
+def pw():
+    i = 2
+    while True:
+        l = tuple(pyprimes.primes_below(i))
+        c = l[-1]
+        if w(i, c, l) > 5000:
+            return i
+        i += 1
 
 
+print pw()
--- code/euler78.py
+++ code/euler78.py
@@ -1,11 +1,45 @@
+import sys
+sys.setrecursionlimit(10 ** 5)
 
+def memo(f): # to cache 
+    cache = {}
+    def _f(*args):
+        try:
+            return cache[args]
+        except KeyError:
+            cache[args] = result = f(*args) 
+            return result
+    return _f
 
+def penta(n):
+    k, p = 1, 0
+    while p <= n:
+        p, s = k * (3 * k - 1) / 2, (-1) ** (k - 1)
+        yield p, s
+        k *= -1
+        p, s = k * (3 * k - 1) / 2, (-1) ** (k - 1)
+        yield p, s
+        k = k * -1 + 1
 
-def ptt(n, g):
-    t = 0
-    t += n
-    n -= 2
+@memo
+def p(n):
+    if n == 0:
+        return 1
+    elif n < 0:
+        return 0
+    else:
+        return sum(s * p(n - pt) for pt, s in penta(n))
+
+def run():
+    n = 1
     while True:
-        t += n
+        if p(n) % 10 ** 6 == 0:
+            return n
+        n += 1
 
-    
+#print run()
+print p(4096)
+
+
+
+
